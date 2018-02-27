726f3ad97968e2e47e4cbac94abbb43c996fd82b
Nacho <iperezpedrajas@gmail.com>
2018-02-27 12:55:53 +0100

    Generate scaffold to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :posts
-  get 'practice/index'
 
+  root 'posts#index'
+  
+  get 'practice/index'
   get 'practice/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
